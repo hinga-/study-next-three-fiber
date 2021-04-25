fe082d9eac247ee846d8953426b8ad7690a375f0
hinga <higa0208@gmail.com>
2021-04-25 14:19:22 +0900

    add: モデルデータ追加 update: package.json

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,39 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { NextPage } from 'next'
-import { Canvas } from 'react-three-fiber'
-import Box from '@/components/Box'
+import dynamic from 'next/dynamic'
+import { Canvas } from '@react-three/fiber'
 import Demo from 'styles/Demo.module.scss'
+// import {OrbitControls} from '@react-three/drei'
+
+const DancingModel = dynamic(() => import('@/components/DancingModel'), {
+  ssr: false,
+})
 
 const Home: NextPage = () => {
   return (
     <div className={Demo.bg}>
-      <Canvas>
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <Box position={[-1.2, 0, 0]} />
-        <Box position={[1.2, 0, 0]} />
+      <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 10], fov: 50 }}>
+        <ambientLight intensity={0.5} />
+        <spotLight
+          intensity={0.3}
+          angle={0.1}
+          penumbra={1}
+          position={[10, 15, 10]}
+          castShadow
+        />
+
+        <mesh
+          receiveShadow
+          rotation={[-Math.PI / 2, 0, 0]}
+          position={[0, -2, 0]}
+        >
+          <planeBufferGeometry attach="geometry" args={[100, 100]} />
+          <meshBasicMaterial attach="material" color={'#f8f8f8'} />
+        </mesh>
+        <Suspense fallback={null}>
+          <DancingModel position={[0, -2, 0]} />
+        </Suspense>
+        {/* <OrbitControls /> */}
       </Canvas>
     </div>
   )
